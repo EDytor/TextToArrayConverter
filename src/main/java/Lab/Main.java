743ffd96f247a835c0049d9df5b6421118a1ba0c
EDyTor <91157109+EDyTor@users.noreply.github.com>
2021-11-24 22:06:34 +0100

    exercises

---
+++ src/main/java/Lab/Main.java
@@ -0,0 +1,9 @@
+package Lab;
+
+public class Main {
+    public static void main(String[] args) {
+        Vector vectorX = new Vector(3.0,4.0,10.0);
+        vectorX.module();
+        vectorX.print();
+    }
+}
