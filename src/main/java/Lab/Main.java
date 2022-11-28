b5da614c150a05c53096c9b5105b0f53b9a98ab7
EDytor <92216550+EDytor@users.noreply.github.com>
2022-11-28 12:51:11 +0100

    Delete src/main/java/Lab directory

--- src/main/java/Lab/Main.java
+++
@@ -1,9 +0,0 @@
-package Lab;
-
-public class Main {
-    public static void main(String[] args) {
-        Vector vectorX = new Vector(3.0,4.0,10.0);
-        vectorX.module();
-        vectorX.print();
-    }
-}
